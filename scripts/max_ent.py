93739f69175ac001a500202b8a58ec9e7664abba
aloctavodia <aloctavodia@gmail.com>
2024-12-13 22:20:39 -0300

    refine imgages

--- scripts/max_ent.py
+++ scripts/max_ent.py
@@ -10,13 +10,13 @@ cons = [[{"type": "eq", "fun": lambda x: np.sum(x) - 1}],
         [{"type": "eq", "fun": lambda x: np.sum(x) - 1},
          {"type": "eq", "fun": lambda x: 30 - np.sum(x * np.arange(0, 120))}],
         [{"type": "eq", "fun": lambda x: np.sum(x) - 1},
-         {"type": "eq", "fun": lambda x: np.sum(x[range(72, 96)]) - 0.7}]]
+         {"type": "eq", "fun": lambda x: np.sum(x[range(60, 90)]) - 0.70}]]
 
 _, axes = plt.subplots(1, 3, figsize=(10, 3), sharey=True, constrained_layout=True)
 
 titles = ["No constraints",
-          "Mean 0.25",
-          "70% of the mass is\n betwen 0.6-0.8"]
+          "Mean=0.25",
+          "70% of the mass\n between 0.5-0.75"]
 
 for i, (c, ax, title) in enumerate(zip(cons, axes, titles)):    
     val = minimize(lambda x: -entropy(x), 
@@ -28,5 +28,6 @@ for i, (c, ax, title) in enumerate(zip(cons, axes, titles)):
     ax.plot(np.linspace(0, 1, 120), val, color=f"C{i}", lw=3)
     ax.set_yticks([])
     ax.set_title(title)
+    ax.axhline(0, color="0.75", ls="--", lw=1)
 
 plt.savefig("../img/max_ent.png")
