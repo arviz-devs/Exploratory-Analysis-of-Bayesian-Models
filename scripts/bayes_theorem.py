041d341cdf71ac63fe50934ad49cd665fd50a585
Osvaldo A Martin <aloctavodia@gmail.com>
2024-12-18 16:04:27 -0300

    expand intro (#84)

---
+++ scripts/bayes_theorem.py
@@ -0,0 +1,48 @@
+import arviz_plots as azp
+import numpy as np
+import preliz as pz
+import matplotlib.pyplot as plt
+
+azp.style.use("arviz-clean")
+
+grid = np.linspace(0, 1, 500)
+prior = pz.Beta(4, 8).pdf(grid)
+likelihood = pz.Binomial(36, grid).pdf(24)
+posterior = likelihood * prior
+prior /= prior.sum()
+likelihood /= likelihood.sum()
+posterior /= posterior.sum()
+_, ax = plt.subplots(sharex=True, figsize=(8, 3))
+ax.plot(grid, prior, '-', label="prior", lw=2)
+ax.plot(grid, likelihood, '-', label="likelihood", lw=2)
+ax.plot(grid, posterior, '-', label="posterior", lw=3)
+
+ax.set_xticks([])
+ax.set_yticks([])
+ax.legend()
+plt.savefig("../img/bayes_theorem_00.png")
+
+
+_, ax = plt.subplots(2, 1, sharex=True,  sharey=True, figsize=(8, 3))
+
+prior_trunc = prior.copy()
+prior_trunc[320:] = 0
+posterior_trunc = likelihood * prior_trunc
+prior_trunc /= prior_trunc.sum()
+posterior_trunc /= posterior_trunc.sum()
+
+ax[0].plot(grid, prior, '-', label="prior", lw=2)
+ax[0].plot(grid, likelihood, '-', label="likelihood", lw=2)
+ax[0].plot(grid, posterior, '-', label="posterior", lw=3)
+
+ax[1].plot(grid, prior_trunc, '-', label="prior", lw=2)
+ax[1].plot(grid, likelihood, '-', label="likelihood", lw=2)
+ax[1].plot(grid, posterior_trunc, '-', label="posterior", lw=3)
+
+ax[0].axvline(320/500,0, 1, ls="--", color="0.5")
+ax[1].axvline(320/500,0, 1, ls="--", color="0.5")
+
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+ax[0].legend()
+plt.savefig("../img/bayes_theorem_01.png")
---
+++ scripts/prior_posterior.py
@@ -0,0 +1,39 @@
+import arviz_plots as azp
+import numpy as np
+import preliz as pz
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+azp.style.use("arviz-clean")
+fig, ax = plt.subplots(1, 1, figsize=(8, 2.5))
+
+grid = np.linspace(0, 1, 500)
+
+likelihood = pz.Binomial(192, grid).pdf(96)
+likelihood /= likelihood.sum()
+
+alphas_0 = list(range(2, 40))
+alphas_1 = list(range(1, 40))
+
+alphas = alphas_0 + alphas_1
+betas = alphas_0[::-1] + alphas_1
+
+
+def update(idx):
+    ax.clear()
+
+    prior = pz.Beta(alphas[idx], betas[idx]).pdf(grid)
+    posterior = likelihood * prior
+    prior /= prior.sum()
+    posterior /= posterior.sum()
+
+    ax.plot(grid, likelihood, '-', label="likelihood", color="k", lw=1)
+    ax.plot(grid, prior, '-', label="prior", color="C0", lw=1)
+    ax.plot(grid, posterior, '-', label="posterior", color="C0", lw=4)
+
+    ax.set_yticks([])
+    ax.set_ylim(0, 0.035)
+    ax.legend(loc="upper right", fontsize=9)
+
+ani = FuncAnimation(fig, update, frames=len(alphas), interval=150, repeat=False)
+ani.save("../img/prior_posterior.gif")
